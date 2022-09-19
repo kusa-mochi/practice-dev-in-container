768bab0b45b9075f04fde5a175d1c7c0d5b35ca7
kusa-mochi <whoatemyapplepie@gmail.com>
2022-09-19 15:54:32 +0900

    modified the script to run docker-compose.

--- create-dev-env.ps1
+++ create-dev-env.ps1
@@ -1,3 +1,3 @@
-docker-compose build
-docker-compose run --rm app sh -c 'npx create-next-app sample-app --typescript'
+docker-compose build --no-cache
+# docker-compose run --rm app sh -c 'npx create-next-app sample-app --typescript'
 docker-compose up
